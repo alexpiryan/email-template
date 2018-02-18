fd00a0d7f0cf8e014d43d7f465bdb16de444c2cf
danimoh <github.danimoh@neverbox.com>
2018-02-18 11:33:19 -0600

    layout changes and adaptions on mobile

--- gulpfile.js
+++ gulpfile.js
@@ -11,44 +11,6 @@ var globalData = {
 };
 */
 
-/* *************
-  CSS
-************* */
-
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var postcssProcessors = [
-    autoprefixer( { browsers: ['last 2 versions', 'ie > 10'] } )
-];
-
-gulp.task('prefixCss', function() {
-    return gulp.src('src/css/inline.css')
-        .pipe(
-            postcss(postcssProcessors)
-        )
-        .pipe(gulp.dest('build/css/'));
-});
-
-
-var inlineCss = require('gulp-inline-css');
-
-gulp.task('inlinecss', ['prefixCss', 'nunjucks'], function() {
-    return gulp.src('build/*.html')
-      /*// dont inline css automatically for now as it removes end tags /> for meta, img and br which leads to
-        // HTML that doesn't pass the html validator anymore. Also strips out fallback background-image declarations.
-        .pipe(
-            inlineCss({
-                applyStyleTags: false,
-                removeStyleTags: false
-            })
-            .on('error', gutil.log)
-        )*/
-        .pipe(gulp.dest('build/'));
-});
-
-
-
-
 
 /* *************
   TEMPLATING
@@ -101,7 +63,7 @@ var filesToWatch = [
 ];
 
 gulp.task('watch', function() {
-    gulp.watch(filesToWatch,['nunjucks', 'inlinecss']);
+    gulp.watch(filesToWatch,['nunjucks']);
 });
 
 
@@ -109,6 +71,4 @@ gulp.task('watch', function() {
     DEFAULT
 ************* */
 
-gulp.task('default', ['nunjucks', 'inlinecss', 'watch']);
-
-
+gulp.task('default', ['nunjucks', 'watch']);
